3b9aa2f0326970dc0c09137a3ac1fd4706fc1e86
Maxime Connat <max@mac-mini-m1.local>
2024-10-01 22:20:02 +0200

    [UPD] deactivate vsync

--- conf.lua
+++ conf.lua
@@ -6,7 +6,7 @@ function love.conf(t)
     t.window.width = 1280                     -- The window width (number)
     t.window.height = 1024                    -- The window height (number)
     t.window.resizable = false                -- Let the window be user-resizable (boolean)
-    t.window.vsync = 1                        -- Vertical sync mode (number)
+    t.window.vsync = 0                        -- Vertical sync mode (number)
     t.window.display = 1                      -- Index of the monitor to show the window in (number)
     t.window.usedpiscale = true               -- Enable automatic DPI scaling when highdpi is set to true as well (boolean)
     t.debug = false                           -- Enable debug mode
